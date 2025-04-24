251bb8ad8012aab3c54f4e1dd0f848cb28e1df1a
cmramsey3 <coltonramsey3@gmail.com>
2025-04-24 13:48:36 -0400

    added comments in main

--- Jimmy_Chitwood_Final_Project/main_package/main.py
+++ Jimmy_Chitwood_Final_Project/main_package/main.py
@@ -17,14 +17,18 @@ from Utilities.movie_decryption import *
 from Utilities.image import *
 
 if __name__ == "__main__":
+    #team_picture = ImageDisplayer("Data/UC_Baseball_Team.jpg")
+    #team_picture.display()
+    
+    # Decrypts and Prints Location Message
     team_name = "Jimmy Chitwood"
     hints_path = "Data/EncryptedGroupHints Spring 2025.json"
     english_path = "Data/UCEnglish.txt"
     decryptor = LocationDecryptor(team_name, hints_path, english_path)
     location = decryptor.decrypt()
     print(f"Decrypted Location:\n{location}\n")
-    #team_picture = ImageDisplayer("Data/UC_Baseball_Team.jpg")
-    #team_picture.display()
+
+    # Decrypts and Prints Movie Results
     movieEncryption = movie_decryption()
     encrypted_data = movieEncryption.get_encrypt()
     movieEncryption.decrypt_data(encrypted_data)
