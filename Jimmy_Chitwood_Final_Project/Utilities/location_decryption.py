117a0ac73e4ee48144ebf136f489d05381c7d379
matthewboutros <matthew.boutros33@gmail.com>
2025-04-24 13:18:34 -0400

    added location_decryption and tentative main

--- Jimmy_Chitwood_Final_Project/Utilities/location_decryption.py
+++ Jimmy_Chitwood_Final_Project/Utilities/location_decryption.py
@@ -16,3 +16,32 @@
 
 
 # Our team name is a key in the dictionary and we use it to match the numbers to words in the txt file to come up with our location
+
+import json
+
+class LocationDecryptor:
+    """
+    A utility class to decrypt campus location data for a project team.
+    It uses 0-based indexing into the English word list provided.
+
+    Attributes:
+        team_name (str): The name of the team whose location is to be decrypted.
+        hints_path (str): Path to the EncryptedGroupHints JSON file.
+        english_path (str): Path to the UCEnglish.txt file.
+    """
+    def __init__(self, team_name, hints_path, english_path):
+        self.team_name = team_name
+        self.hints_path = hints_path
+        self.english_path = english_path
+
+    def decrypt(self):
+        """Returns the decrypted location string."""
+        with open(self.hints_path, "r") as f:
+            encrypted_data = json.load(f)[self.team_name]
+
+        with open(self.english_path, "r") as f:
+            words = f.readlines()
+
+        # Use 0-based indexing (DO NOT subtract 1)
+        decrypted_words = [words[int(index)].strip() for index in encrypted_data]
+        return " ".join(decrypted_words)
